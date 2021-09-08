15e3f7377f853e3432f845f47c5fcaf54f2e1366
rafaelturse <rafaelturse@hotmail.com>
2021-09-07 20:06:15 -0400

    helloJava

---
+++ workspace/simpleschool/src/main/java/com/rafaelturse/simpleschool/SimpleschoolApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelturse.simpleschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleschoolApplication.class, args);
+	}
+
+}
---
+++ workspace/simpleschool/src/test/java/com/rafaelturse/simpleschool/SimpleschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelturse.simpleschool;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
